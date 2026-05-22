4aee9160ccf9fadb1c28c4840997115bda7c310b
Juan Carlos <jsegura253@gmail.com>
2026-05-22 15:42:18 -0600

    comenzando vistas

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; 2026 Mi Proyecto</p>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <h1>Mi Proyecto</h1>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Inicio</a></li>
+            <li><a href="{{ route('clientes.index') }}">Clientes</a></li>
+            <li><a href="{{ route('consultorias.index') }}">Consultorías</a></li>
+            <li><a href="{{ route('solicitudes.index') }}">Solicitudes</a></li>
+            <li><a href="{{ route('usuarios.index') }}">Usuarios</a></li>
+        </ul>
+    </nav>
+</header>
